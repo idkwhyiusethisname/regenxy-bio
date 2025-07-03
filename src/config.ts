e1f8cc1c0f26ae7fb063dea9fe9dcfd3a7145c0e
idkwhyiusethisname <kikiasas319@gmail.com>
2025-07-03 22:11:12 +0700

    2

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Regenxyy",
-	subtitle: "an Regenxy skibdi toliet",
+	subtitle: "an 13 year old student",
 	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 315, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
